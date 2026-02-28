1c6c07377791648b237fea831767026ef18e1c64
MohammadTuhin1 <muhammadtuhin.dev@gmail.com>
2026-02-28 14:23:47 +0600

    initial commit

---
+++ lib/APILive/crud.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Crud extends StatelessWidget {
+  const Crud({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
